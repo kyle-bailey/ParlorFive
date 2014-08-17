fa206e68f6cc028986a505b2a57c7bc7dfed0391
Kyle <kyle.bailey@ymail.com>
2014-08-17 11:46:30 -0500

    Working on image attachment in the work upload form, and trying to add League Gothic font

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -5,7 +5,7 @@ class WorksController < ApplicationController
 	end
 
 	def new
-		@work = Work.new #Can't save - need to do a two step process. Save the form for the image, make sure its correct. Once that is saved, then attach the image
+		@work = Work.new
 		@user = current_user
 	end
 
